1e081dea0ff60daa969964ae08f68ce00b9e04c2
Tlili Mourad <mourad.tlili@esprit.tn>
2021-12-23 13:07:49 +0100

    arge

--- Tests/collect.ts
+++ Tests/collect.ts
@@ -18,7 +18,7 @@ const router = new Router();
 const app = new Application();
 let PORT = Deno.env.get("PORT") || 80;
 
-//const s = serve({ port: argPort ? Number(argPort) : PORT }); //For Heroku
+const s = serve({ port: argPort ? Number(argPort) : PORT }); //For Heroku
 
 // Starting the server
 
